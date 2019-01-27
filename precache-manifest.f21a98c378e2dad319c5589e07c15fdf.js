8dbf0c8b8443d9a1e6684b4d075129c5d26ed019
thaoho <thao117815@gmail.com>
2019-01-27 11:53:14 +0700

    Add files via upload

---
+++ precache-manifest.f21a98c378e2dad319c5589e07c15fdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ed82db24e7e5012c53c",
+    "url": "/static/css/main.a3316494.chunk.css"
+  },
+  {
+    "revision": "0ed82db24e7e5012c53c",
+    "url": "/static/js/main.0ed82db2.chunk.js"
+  },
+  {
+    "revision": "effe1b9996f7dcea4ddd",
+    "url": "/static/js/1.effe1b99.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50eb49fdcc9bdee337389ff1de3e3837",
+    "url": "/index.html"
+  }
+];
